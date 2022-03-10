6273d5ce8264c3798655f14d29810307e457c92b
Robert Wittman <robkwittman@gmail.com>
2022-03-10 16:15:58 -0500

    Upgrade to 0.0.1-rc4

--- Formula/launchbox.rb
+++ Formula/launchbox.rb
@@ -1,8 +1,8 @@
 class Launchbox < Formula
   desc "Launchbox CLI client"
   homepage "https://launchbox.io/"
-  url "https://github.com/launchboxio/launchbox/archive/refs/tags/0.0.1-rc3.tar.gz"
-  sha256 "7a5cf70af05352485ccd9170975c9bede0aa2a5a4257dbdbd494e1297d0556cd"
+  url "https://github.com/launchboxio/launchbox/archive/refs/tags/0.0.1-rc4.tar.gz"
+  sha256 "35f2df63a32134f432a04ddca5df5004c1a772f0a125457a20f2588ad962b1bd"
 
   depends_on "go" => :build
 
